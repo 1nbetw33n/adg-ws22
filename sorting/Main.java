daae4be591a849fbc1bfb93ee0eede6c0472ff3c
inbetween <a.ab@mail.de>
2023-03-04 07:28:45 +0100

    Modularized the code. Added support for generating random data.

--- sorting/Main.java
+++ sorting/Main.java
@@ -49,6 +49,8 @@ import java.util.Scanner;
 final class Main {
 
         private final List<String> sorting_algorithms;
+        private Object[] data;
+        private String algo;
         private final Scanner sc;
 
         /**
@@ -60,9 +62,35 @@ final class Main {
         }
 
         /**
-         * Lists all available sorting algorithms.
+         * Generates a random array of integers that will be sorted.
          */
-        private void show_selection() {
+        private void generate_random_data() {
+                System.out.println("Enter the size of the data ::");
+                var size = sc.nextInt();
+                System.out.println("Enter the range of the data ::");
+                var range = sc.nextInt();
+                data = Util.generate_random_data(Integer.class, size, range);
+                Util.print_array(data);
+                //for some reason the scanner does not work for generating data - so I had to assign it twice to make it work
+                algo = sc.nextLine();
+        }
+
+        /**
+         * Lets the user enter the data they want to sort.
+         */
+        private void use_custom_data() {
+                System.out.println(System.lineSeparator() + "Enter the data you want to sort in the following format ::"
+                        + System.lineSeparator() + "{1,2,3} or {1, 2, 3}" + System.lineSeparator());
+                var data_str = sc.nextLine();
+                Util.is_valid_sorting_input(data_str);
+                data = String_Parser.to_integer_array(data_str);
+        }
+
+        /**
+         * Displays all available sorting algorithms and lets the user choose one.
+         * @param main instance of the main class
+         */
+        private void select_algo(final Main main){
                 System.out.println(System.lineSeparator() + "Which sorting algorithm do you want to use?");
                 System.out.println("Available algorithms ::");
                 sorting_algorithms.stream().map(v -> {
@@ -70,6 +98,9 @@ final class Main {
                         return (i + 1) + " -> " + v;
                 }).forEach(System.out::println);
                 System.out.println("Enter number ::" + System.lineSeparator());
+                algo = sc.next();
+                System.out.println();
+                System.out.println("-> " + main.get_algo(algo).getClass().getSimpleName() + " Sort" + System.lineSeparator());
         }
 
         /**
@@ -77,7 +108,7 @@ final class Main {
          * @param name the name of the sorting algorithm
          * @return the instance of the sorting algorithm
          */
-        private Sort get_sorting_algorithm(String name) {
+        private Sort get_algo(String name) {
                 return switch (name) {
                         case "1" -> new Selection();
                         case "2" -> new Insertion();
@@ -90,23 +121,31 @@ final class Main {
         }
 
         /**
+         * Lets the user choose between generating random data or entering their own data and then lets them choose the sorting algorithm.<br>
+         * The data will then be sorted using the chosen algorithm.
+         */
+        private void select(final Main main) {
+                System.out.println("Do you want to generate random data? [Y/n] ::");
+                var answer = sc.nextLine();
+                if (answer.equals("y")) {
+                        generate_random_data();
+                } else if (answer.equals("n")) {
+                        use_custom_data();
+                } else {
+
+                        throw new IllegalArgumentException("Invalid answer :: " + "'" + answer + "'");
+                }
+                select_algo(main);
+                Objects.requireNonNull(main.get_algo(algo))
+                        .sort((Integer[]) data);
+        }
+
+        /**
          * Runs the program.
          */
         private void run() {
                 var main = new Main();
-                main.show_selection();
-                var algo = sc.nextLine();
-                if (main.get_sorting_algorithm(algo) == null) {
-                        System.out.println("Invalid sorting algorithm!");
-                        return;
-                }
-                System.out.println(System.lineSeparator() + "Enter the data you want to sort in the following format ::"
-                        + System.lineSeparator() + "{1,2,3} or {1, 2, 3}" + System.lineSeparator());
-                var data = sc.nextLine();
-                System.out.println();
-                Util.is_valid_sorting_input(data);
-                Objects.requireNonNull(main.get_sorting_algorithm(algo))
-                        .sort(String_Parser.to_integer_array(data));
+                main.select(main);
         }
 
         public static void main(String[] args) {
